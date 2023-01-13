f8c3a05b2da24cf18c63adb8b37d17778cd2ee63
vincenzoarena <penninoematita@gmail.com>
2023-01-13 13:52:07 +0100

    README.md file added in the main directory and images directory created in public

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# mern-paradise-island
+# Paradise Island
+
+**[Test this app yourself](github.io)**
+
+---
+
+![Screenshot from Project](./images/project.png)
+
+### About Project
+
+lorem ........
+
+### How to use this Project
+
+lorem ........
+
+##### Developed With
+
+- [x] _HTML5_
+- [ ] _CSS3_
+- [x] _SASS_
+- [x] _SCSS_
+- [x] _JavaScript_
+- [ ] _React_
+- [ ] _Bootstrap_
+- [x] _npm_
+- [ ] _..._
+
+---
+
+### Contact
+
+Mail: <penninoematita@gmail.com><br>
+GitHub: [vincenzoarena](https://github.com/vincenzoarena)<br>
+LinkedIn: [Vincenzo Arena](https://www.linkedin.com/in/vincenzo-arena-032a064b/)
+
+---
+
+### Used Tools
+
+- [icons](https://)
+- [Canva](https://www.canva.com/)
+- [npm](https://www.npmjs.com/)
+- [Google Fonts](https://fonts.google.com/)
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [ColorZilla](https://www.colorzilla.com/chrome/)
+
+---
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg', // 679px * 829px
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt',  
+
+        },
+        {
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 250,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'High quality product',  
+
+        },
+        {
+            name: 'Nike Slim Pants',
+            slug: 'nike-slim-pants',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 25,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'High quality product',  
+
+        },
+        {
+            name: 'Adidas fit Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt',  
+
+        },
+    ]
+}
+
+export default data;
