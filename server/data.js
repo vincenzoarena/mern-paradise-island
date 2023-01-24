e4dcbeb2ba08dbe3b380a77362a7c377f5debf30
vincenzoarena <penninoematita@gmail.com>
2023-01-24 14:40:36 +0100

    Add to Cart function implemented.

--- server/data.js
+++ server/data.js
@@ -1,6 +1,7 @@
 const data = {
     products: [
         {
+            _id: '1',
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt',
             category: 'Shirts',
@@ -14,6 +15,7 @@ const data = {
 
         },
         {
+            _id: '2',
             name: 'Adidas Fit Shirt',
             slug: 'adidas-fit-shirt',
             category: 'Shirts',
@@ -27,6 +29,7 @@ const data = {
 
         },
         {
+            _id: '3',
             name: 'Nike Slim Pants',
             slug: 'nike-slim-pants',
             category: 'Pants',
@@ -40,6 +43,7 @@ const data = {
 
         },
         {
+            _id: '4',
             name: 'Adidas fit Shirt',
             slug: 'nike-slim-shirt',
             category: 'Shirts',
