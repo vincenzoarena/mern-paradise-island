22510208b0af8ee30e5737e469ce769fa7a97b4f
vincenzoarena <penninoematita@gmail.com>
2023-01-27 16:43:45 +0100

    Signin implemented but not working

--- server/data.js
+++ server/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+          name: 'Gills',
+          email: 'admin@example.com',
+          password: bcrypt.hashSync('123456'),
+          isAdmin: true,
+        },
+        {
+          name: 'Carlo',
+          email: 'user@example.com',
+          password: bcrypt.hashSync('123456'),
+          isAdmin: false,
+        },
+      ],
     products: [
         {
             // _id: '1',
